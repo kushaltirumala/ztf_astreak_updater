a48f1a2b397499b1d5b89eb115004d4197c69888
kushaltirumala <kushaltirumala99@gmail.com>
2018-03-31 03:04:12 -0700

    helper function for creating webpage

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import datetime
 from StringIO import StringIO
 import json
 
+def create_html(images):
+    with open("index.html", "w") as f:
+        msg = """<!DOCTYPE html><head><title>Skipped images</title><link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'></head><body style='text-align:center; font-family: "Raleway", sans-serif;'><h1 style='margin:50px;'>Skipped images</h1>"""
+        for img in images:
+            msg += """<div style='float:left; margin:20px; padding: 10px;'><img height=200 width=200 src='""" + img + """'></div>"""
+        msg += """</body>"""
+        print msg
+        f.write(msg)
+
 project_link = 'aschig/ztf-astreaks'
 # project_link = 'kushaltirumala/test-2'
 export_type = 'classifications'
@@ -49,3 +58,25 @@ print df['classification_name'].value_counts()
 
 # generate index.html of skipped images
 skips = df[df['classification_name']=="Skip (Includes 'Not Sure' and seemingly 'Blank Images')"]
+
+# will fill with actual images srcs from subject set
+# image_srcs = [
+#     "https://panoptes-uploads.zooniverse.org/production/subject_location/94362a72-3597-4640-add6-e1b3ce4ec3a6.jpeg",
+#     "https://panoptes-uploads.zooniverse.org/production/subject_location/d6d70aba-bf36-4c82-ace7-30ebf791b3fb.jpeg",
+#     "https://panoptes-uploads.zooniverse.org/production/subject_location/f4c91f1b-037e-44d6-95d5-83fed4e00879.jpeg"
+# ]
+# create_html(image_srcs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
